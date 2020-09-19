d991f6380082d82b4e91cd007dd38a40f63edcd0
Quangtest <quangdeeptry7@gmail.com>
2020-09-19 09:42:30 +0700

    edit mains.js

--- mains.js
+++ mains.js
@@ -1,17 +1,22 @@
-const person = [{
-        id: 1,
-        isActive: true
-    },
-    {
-        id: 2,
-        isActive: true
-    },
-    {
-        id: 3,
-        isActive: false
-    },
+const person = [
+  {
+    id: 1,
+    isActive: true,
+  },
+  {
+    id: 2,
+    isActive: true,
+  },
+  {
+    id: 3,
+    isActive: false,
+  },
+  {
+    id: 4,
+    isActive: false,
+  },
 ];
 
-const activePersion = person.filter(job => job.isActive);
+const activePersion = person.filter((job) => job.isActive);
 
 console.log(activePersion);
